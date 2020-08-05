0337afe584713791e2afe77ad5a0affc334a03c5
unknown <ronldmhrb@gmail.com>
2020-08-05 07:55:03 +0530

    Logistic Regression added

--- Regression/regression.py
+++ Regression/regression.py
@@ -7,6 +7,7 @@ class Regression:
         ones = ones.reshape(-1,1) # remove the next line  if it's for linear regression but it's gonna be used for other regression
         return np.append(ones,X,axis = 1)
 
+    # fit works for linear regression and multiple regression
     def fit(self,X,Y,no_of_iter = 10000,alpha = 0.001):
         X = self.modify(X)
         self.theta = np.random.randn(X.shape[1])
@@ -35,3 +36,26 @@ class Regression:
 
     def poly_predict(self,X):
         self.predict(self.polynomial_feature(X))
+    
+    # for logistic regression
+    def sigmoid(self,Z):
+        return 1/(1+np.exp(-Z))
+    
+    def logistic_regression_fit(self,X,Y,no_of_iter = 10000,alpha = 0.001):
+        X = self.modify(X)
+        self.theta = np.random.randn(X.shape[1])
+        for _ in range(no_of_iter):
+            temp = self.sigmoid(np.matmul(X,self.theta))
+            temp = temp - Y
+            self.theta = self.theta-(alpha/X.shape[0])*(X.T.dot(temp))
+
+    def logistic_regression_predict(self,X):
+        X = self.modify(X)
+        X = self.sigmoid(np.matmul(X,self.theta))
+        for i in range(X.shape[0]):
+            if X[i] < 0.5:
+                X[i] = 0
+            else:
+                X[i] = 1
+        return X
+    
